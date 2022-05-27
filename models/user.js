3226e66bc98b093330c07fd884f88dcfae55c281
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-05-27 11:31:02 +0530

    created userSchema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,39 @@
 const mongoose = require("mongoose")
 const crypto = requrie("crypto")
 
-const uuidv1 = require("u")
+const uuidv1 = require("uuid/dist/v1")
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+    history: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+)

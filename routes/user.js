fbdce567ede3af18e835581ce01403a18016d892
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-05-28 00:44:05 +0530

    debugged

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const { signup } = require("../controllers/user")
+const { signup, signin } = require("../controllers/user")
 const { userSignupValidator } = require("../validator/index")
 
 //Routes

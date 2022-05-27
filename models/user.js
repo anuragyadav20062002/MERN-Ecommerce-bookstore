4eda94622557e30ece0f5bec9ece0fbbdc13f037
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-05-27 18:39:09 +0530

    added error handler for same email

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
-      maxlength: 32,
+      unique: true,
     },
     hashed_password: {
       type: String,

22fd842a64d4276126754dad90e46dad4ea9aa0f
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-05-27 11:24:33 +0530

    added crypto module

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const { sayHi } = require("./controllers/user")
+const { sayHi } = require("../controllers/user")
 
 //Routes
 router.get("/", sayHi)

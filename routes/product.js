4116162d881f45351caa217327b83aa2a2ca6e5b
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-05-31 22:12:48 +0530

    added controller to update product

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const router = express.Router()
-const { create, productById, read, remove } = require("../controllers/product")
+const {
+  create,
+  productById,
+  read,
+  remove,
+  update,
+} = require("../controllers/product")
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth")
 const { userById } = require("../controllers/user")
 
@@ -8,6 +14,7 @@ const { userById } = require("../controllers/user")
 router.get("/product/:productId", read)
 router.post("/product/create/:userId", requireSignin, isAdmin, create)
 router.delete("/product/:productId/:userId", requireSignin, isAdmin, remove)
+router.put("/product/:productId/:userId", requireSignin, isAdmin, update)
 
 router.param("userId", userById)
 router.param("productId", productById)

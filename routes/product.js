56825dc4ac26a85589b2ba1dda33ad96779d87ea
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-02 23:41:26 +0530

    added a controller to search products using filters

--- routes/product.js
+++ routes/product.js
@@ -9,6 +9,7 @@ const {
   list,
   listRelated,
   listCategories,
+  listBySearch,
 } = require("../controllers/product")
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth")
 const { userById } = require("../controllers/user")
@@ -21,6 +22,7 @@ router.put("/product/:productId/:userId", requireSignin, isAdmin, update)
 router.get("/products", list)
 router.get("/products/related/:productId", listRelated)
 router.get("/products/categories", listCategories)
+router.post("/products/by/search", listBySearch)
 
 router.param("userId", userById)
 router.param("productId", productById)

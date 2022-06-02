7304a935fb004daf17fcb1b97fabbfbaba7eda46
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-02 23:47:23 +0530

    added a controller to send photo of any product

--- controllers/product.js
+++ controllers/product.js
@@ -257,3 +257,12 @@ exports.listBySearch = (req, res) => {
       })
     })
 }
+
+exports.photo = (req, res, next) => {
+  if (req.product.photo.data) {
+    res.set("Content-Type", req.product.photo.contentType)
+    return res.send(req.product.photo.data)
+  }
+
+  next()
+}

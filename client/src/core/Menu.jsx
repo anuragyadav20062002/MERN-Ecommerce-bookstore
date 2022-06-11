35daf96956f772f0bb3861eb4982066add0c37e4
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-11 12:30:30 +0530

    added menu

--- client/src/core/Menu.jsx
+++ client/src/core/Menu.jsx
@@ -1,8 +1,44 @@
 import React from "react"
 import { Link, withRouter } from "react-router-dom"
 
-const Menu = () => {
-  return <div>Menu</div>
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#00FFAB" }
+  } else {
+    return { color: "#ffffff" }
+  }
 }
 
-export default Menu
+const Menu = ({ history }) => {
+  return (
+    <>
+      <ul className="nav nav-tabs bg-dark">
+        <li className="nav-item">
+          <Link className="nav-link" style={isActive(history, "/")} to="/">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className="nav-link"
+            style={isActive(history, "/signin")}
+            to="/signin"
+          >
+            SignIn
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className="nav-link"
+            style={isActive(history, "/signup")}
+            to="/signup"
+          >
+            SignUp
+          </Link>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default withRouter(Menu)

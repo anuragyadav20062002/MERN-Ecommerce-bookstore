56423f29cb32ab5d9d3379b54a87d28529c4f09b
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-11 13:49:53 +0530

    added envioenment variables

--- client/src/user/Signup.jsx
+++ client/src/user/Signup.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import Layout from "../core/Layout"
+import { API } from "../config"
 
 const Signup = () => {
   return (
     <>
       <Layout title="Sign-Up" description="Sign Up to E-Bookstore">
-        ...
+        {API}
       </Layout>
     </>
   )

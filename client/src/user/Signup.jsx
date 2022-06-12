2ba080080549ed12059c73ffa39b00c97c084a9e
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-12 22:49:59 +0530

    added a state to store and update values

--- client/src/user/Signup.jsx
+++ client/src/user/Signup.jsx
@@ -1,24 +1,48 @@
 /* eslint-disable no-unused-vars */
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../core/Layout"
 import { API } from "../config"
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  })
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value })
+  }
+
   const signUpForm = () => {
     return (
       <div>
         <form>
           <div className="form-group">
             <label className="text-muted">Name</label>
-            <input type="text" className="form-control" />
+            <input
+              onChange={handleChange("name")}
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label className="text-muted">Email</label>
-            <input type="email" className="form-control" />
+            <input
+              onChange={handleChange("email")}
+              type="email"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label className="text-muted">Password</label>
-            <input type="password" className="form-control" />
+            <input
+              onChange={handleChange("password")}
+              type="password"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-danger">Submit</button>
         </form>
@@ -34,6 +58,7 @@ const Signup = () => {
         className="container col-md-8 offset-md-2"
       >
         {signUpForm()}
+        {/* {JSON.stringify(values)} */}
       </Layout>
     </>
   )

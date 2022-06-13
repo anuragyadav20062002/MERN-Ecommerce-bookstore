5558dbe83e0e7f9a035a6f697287de271f8019e0
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-13 10:58:54 +0530

    added a api function for signin in auth.js

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -13,3 +13,20 @@ export const signup = (user) => {
     })
     .catch((err) => {})
 }
+
+////signin////
+export const signin = (user) => {
+  console.log(user)
+  return fetch(`http://localhost:8000/api/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => {})
+}

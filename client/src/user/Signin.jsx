645dd4f3ae30f481405e40ac75ad3ad5dd9a9990
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-13 12:36:58 +0530

    created signin form

--- client/src/user/Signin.jsx
+++ client/src/user/Signin.jsx
@@ -1,11 +1,104 @@
-import React from "react"
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react"
 import Layout from "../core/Layout"
+import { API } from "../config"
+import { Link, Redirect } from "react-router-dom"
+import { signin } from "../auth"
 
 const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    redirectToReferrer: false,
+  })
+
+  const { email, password, loading, error, redirectToReferrer } = values
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value })
+  }
+
+  const clickSubmit = (event) => {
+    event.preventDefault()
+    setValues({ ...values, error: false, loading: true })
+    signin({ email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false })
+      } else {
+        setValues({
+          ...values,
+          redirectToReferrer: true,
+        })
+      }
+    })
+  }
+
+  const signInForm = () => {
+    return (
+      <div>
+        <form>
+          <div className="form-group">
+            <label className="text-muted">Email</label>
+            <input
+              onChange={handleChange("email")}
+              type="email"
+              className="form-control"
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            <label className="text-muted">Password</label>
+            <input
+              onChange={handleChange("password")}
+              type="password"
+              className="form-control"
+              value={password}
+            />
+          </div>
+          <button onClick={clickSubmit} className="btn btn-danger">
+            Submit
+          </button>
+        </form>
+      </div>
+    )
+  }
+
+  const showLoading = () =>
+    loading && (
+      <div className="aler alert-info">
+        <h2>Loading...</h2>
+      </div>
+    )
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      return <Redirect to="/" />
+    }
+  }
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      <h2> {error}</h2>
+    </div>
+  )
+
   return (
     <>
-      <Layout title="Sign-In" description="Please Sign-In To E-Bookstore">
-        ...
+      <Layout
+        title="Sign-Up"
+        description="Sign Up to E-Bookstore"
+        className="container col-md-8 offset-md-2"
+      >
+        {showLoading()}
+        {showError()}
+        {signInForm()}
+        {redirectUser()}
+        {/* {JSON.stringify(values)} */}
       </Layout>
     </>
   )

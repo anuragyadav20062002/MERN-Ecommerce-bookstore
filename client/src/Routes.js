af1a6970baade3feaee7097f52346f6242725874
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-15 12:57:37 +0530

    Private route for admin dashboard

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -6,7 +6,9 @@ import Signup from "./user/Signup"
 import Home from "./core/Home"
 import Menu from "./core/Menu"
 import PrivateRoute from "./auth/PrivateRoute"
-import Dashborad from "./user/UserDashboard"
+import UserDashboard from "./user/UserDashboard"
+import AdminRoute from "./auth/AdminRoute"
+import AdminDashboard from "./user/AdminDashboard"
 
 const Routes = () => {
   return (
@@ -16,7 +18,8 @@ const Routes = () => {
         <Route path="/signin" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
         <Route path="/" exact component={Home} />
-        <PrivateRoute path="/user/dashboard" exact component={Dashborad} />
+        <PrivateRoute path="/user/dashboard" exact component={UserDashboard} />
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
       </Switch>
     </BrowserRouter>
   )

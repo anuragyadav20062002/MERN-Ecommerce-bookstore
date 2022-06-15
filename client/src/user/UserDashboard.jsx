0e640eb46b733d4cc48e839faccee4c7c39cc77d
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-15 11:43:47 +0530

    displayed user information in dashboard

--- client/src/user/UserDashboard.jsx
+++ client/src/user/UserDashboard.jsx
@@ -1,10 +1,35 @@
 import React from "react"
 import Layout from "../core/Layout"
+import { isAuthenticated } from "../auth"
 
 const UserDashboard = () => {
+  const {
+    user: { _id, name, email, role },
+  } = isAuthenticated()
+
   return (
-    <Layout title="User DashBorad" description="User Dashboard">
-      ...
+    <Layout
+      title="User DashBorad"
+      description="User Dashboard"
+      className="container"
+    >
+      <div className="card mb-5" style={{ fontWeight: "bold" }}>
+        <h3 className="card-header"> User Information</h3>
+        <ul className="list-group">
+          <li className="list-group-item">Name : {name}</li>
+          <li className="list-group-item">Email : {email}</li>
+          <li className="list-group-item">
+            Role : {role === 1 ? "Admin" : "Registered User"}
+          </li>
+        </ul>
+      </div>
+
+      <div className="card mb-5" style={{ fontWeight: "bold" }}>
+        <h3 className="card-header">Purchase History</h3>
+        <ul className="list-group">
+          <li className="list-group-item">history</li>
+        </ul>
+      </div>
     </Layout>
   )
 }

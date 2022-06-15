72dfcd266bc437ad620471923ff25779402d0d65
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-15 12:29:51 +0530

    created admin Dashboard

--- client/src/user/AdminDashboard.jsx
+++ client/src/user/AdminDashboard.jsx
@@ -4,24 +4,24 @@ import Layout from "../core/Layout"
 import { isAuthenticated } from "../auth"
 import { Link } from "react-router-dom"
 
-const UserDashboard = () => {
+const AdminDashboard = () => {
   const {
     user: { _id, name, email, role },
   } = isAuthenticated()
 
-  const userLinks = () => {
+  const adminLinks = () => {
     return (
       <div className="card bg-success mb-5" style={{ fontWeight: "bold" }}>
-        <h4 className="card-header">User Links</h4>
+        <h4 className="card-header">Admin Links</h4>
         <ul className="list-group">
           <li className="list-group-item">
-            <Link className="nav-link" to="/cart">
-              My Cart
+            <Link className="nav-link" to="/create/category">
+              Crete Category
             </Link>
           </li>
           <li className="list-group-item">
-            <Link className="nav-link" to="/profile/update">
-              Update Profile
+            <Link className="nav-link" to="/create/product">
+              Create Product
             </Link>
           </li>
         </ul>
@@ -29,27 +29,16 @@ const UserDashboard = () => {
     )
   }
 
-  const userInfo = () => {
+  const adminInfo = () => {
     return (
       <div className="card bg-primary mb-5" style={{ fontWeight: "bold" }}>
-        <h3 className="card-header"> User Information</h3>
+        <h3 className="card-header"> Admin Information</h3>
         <ul className="list-group ">
           <li className="list-group-item">Name : {name}</li>
           <li className="list-group-item">Email : {email}</li>
           <li className="list-group-item">
             Role : {role === 1 ? "Admin" : "Registered User"}
           </li>
-        </ul>
-      </div>
-    )
-  }
-
-  const userHistory = () => {
-    return (
-      <div className="card bg-warning mb-5" style={{ fontWeight: "bold" }}>
-        <h3 className="card-header">Purchase History</h3>
-        <ul className="list-group">
-          <li className="list-group-item">history</li>
         </ul>
       </div>
     )
@@ -62,14 +51,11 @@ const UserDashboard = () => {
       className="container"
     >
       <div className="row">
-        <div className="col-3">{userLinks()}</div>
-        <div className="col-9">
-          {userInfo()}
-          {userHistory()}
-        </div>
+        <div className="col-3">{adminLinks()}</div>
+        <div className="col-9">{adminInfo()}</div>
       </div>
     </Layout>
   )
 }
 
-export default UserDashboard
+export default AdminDashboard

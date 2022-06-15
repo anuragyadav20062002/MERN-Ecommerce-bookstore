4cc7c6c96da6de4974b8810dc49568b92ed07a70
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-15 14:57:14 +0530

    added fuction to send api request to backend to create backend and initalized addproduct component

--- client/src/admin/apiadmin.js
+++ client/src/admin/apiadmin.js
@@ -1,5 +1,5 @@
 export const createCategory = (userId, token, category) => {
-  console.log(userId, token, category)
+  console.log(category)
   return fetch(`http://localhost:8000/api/category/create/${userId}`, {
     method: "POST",
     headers: {
@@ -8,6 +8,21 @@ export const createCategory = (userId, token, category) => {
       Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(category),
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => {})
+}
+export const createProduct = (userId, token, product) => {
+  console.log(product)
+  return fetch(`http://localhost:8000/api/product/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: product,
   })
     .then((response) => {
       return response.json()

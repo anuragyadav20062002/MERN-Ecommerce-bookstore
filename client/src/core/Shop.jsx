d711f4de34c239c0952d17d43de3e5a6c8e3e176
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-16 22:42:40 +0530

    added some fixes price ranges in a js file

--- client/src/core/Shop.jsx
+++ client/src/core/Shop.jsx
@@ -4,6 +4,8 @@ import Layout from "./Layout"
 import Card from "./Card"
 import { getCategories } from "./apiCore"
 import Checkbox from "./Checkbox"
+import { prices } from "./fixedPrices"
+
 const Shop = () => {
   const [myFilters, setMyFilters] = useState({
     filters: { category: [], price: [] },

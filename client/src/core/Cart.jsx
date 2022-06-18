a7487da01efff155f9b90d5dbda11581346b97d7
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-18 18:39:39 +0530

    successfully created Cart component to show items in cart

--- client/src/core/Cart.jsx
+++ client/src/core/Cart.jsx
@@ -6,7 +6,46 @@ import { getCart } from "./cartHelpers"
 import Card from "./Card"
 
 const Cart = () => {
-  return <div></div>
+  const [items, setitems] = useState([])
+
+  useEffect(() => {
+    //populating items state when page refreshes
+    setitems(getCart())
+  }, [])
+
+  const showItems = (items) => {
+    return (
+      <div>
+        <h2>Your Cart Has {`${items.length}`} items</h2>
+        <hr />
+        {items.map((item, i) => (
+          <Card key={i} product={item} />
+        ))}
+      </div>
+    )
+  }
+
+  const noItemsMessage = () => (
+    <h2>
+      Your Cart Is Empty. <br /> <Link to="/shop">Continue Shopping</Link>
+    </h2>
+  )
+
+  return (
+    <>
+      <Layout
+        title="Shopping Cart 🛒"
+        description="Manage your cart items. Add, remove , checkout or continue shopping!!"
+        className="container-fluid"
+      >
+        <div className="row">
+          <div className="col-6">
+            {items.length > 0 ? showItems() : noItemsMessage()}
+          </div>
+        </div>
+      </Layout>
+    </>
+  )
 }
 
 export default Cart

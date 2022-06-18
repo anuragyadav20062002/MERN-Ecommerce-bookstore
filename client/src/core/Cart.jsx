df8e4b92d5aaea19b7d4a6723fdd95d8bd3ea47b
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-18 18:26:17 +0530

    initialized cart component

--- client/src/core/Cart.jsx
+++ client/src/core/Cart.jsx
@@ -1,4 +1,9 @@
-import React from "react"
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import Layout from "./Layout"
+import { getCart } from "./cartHelpers"
+import Card from "./Card"
 
 const Cart = () => {
   return <div></div>

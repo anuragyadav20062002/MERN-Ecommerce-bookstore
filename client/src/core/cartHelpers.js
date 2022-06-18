3692a06e9eb92ccfe9a2a08cc477dfaa42da0da1
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-18 18:24:49 +0530

    added a function to get all items in cart

--- client/src/core/cartHelpers.js
+++ client/src/core/cartHelpers.js
@@ -36,3 +36,13 @@ export const itemTotal = () => {
     return 0
   }
 }
+
+export const getCart = () => {
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("cart")) {
+      return JSON.parse(localStorage.getItem("cart"))
+    }
+
+    return 0
+  }
+}

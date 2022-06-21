f900590f1859df2f7cc22ad90ffd6b110fc95123
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-21 18:34:33 +0530

    added a controller to add purchase history in user model

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const router = express.Router()
 const { requireSignin, isAuth } = require("../controllers/auth")
-const { userById } = require("../controllers/user")
+const { userById, addOrderToUserhistory } = require("../controllers/user")
 const { create } = require("../controllers/order")
 
-router.post("/order/create/:userId", requireSignin, isAuth, create)
+router.post(
+  "/order/create/:userId",
+  requireSignin,
+  isAuth,
+  addOrderToUserhistory,
+  create
+)
 
 router.param("userId", userById)
 module.exports = router

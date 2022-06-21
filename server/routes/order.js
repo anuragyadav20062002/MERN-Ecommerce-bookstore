32f4707c72ea5a63233e0082b9c22a4a7ae86202
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-21 23:14:35 +0530

    added a function to get all enum values

--- server/routes/order.js
+++ server/routes/order.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth")
 const { userById, addOrderToUserhistory } = require("../controllers/user")
 const { decreaseQuantity } = require("../controllers/product")
-const { create, listOrders } = require("../controllers/order")
+const { create, listOrders, getStatusValues } = require("../controllers/order")
 
 router.post(
   "/order/create/:userId",
@@ -15,6 +15,13 @@ router.post(
 )
 
 router.get("/order/list/:userId", requireSignin, isAuth, isAdmin, listOrders)
+router.get(
+  "/order/status-values/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  getStatusValues
+)
 
 router.param("userId", userById)
 module.exports = router

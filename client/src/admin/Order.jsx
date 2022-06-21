c9a3ba800f2077c8ae6fd631834d259f4ce1749f
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-21 23:30:47 +0530

    dixed errors shown all enum values in frontend

--- client/src/admin/Order.jsx
+++ client/src/admin/Order.jsx
@@ -4,11 +4,12 @@ import React, { useState, useEffect } from "react"
 import Layout from "../core/Layout"
 import { isAuthenticated } from "../auth"
 import { Link } from "react-router-dom"
-import { listOrders } from "./apiadmin"
+import { listOrders, getStatusValues } from "./apiadmin"
 import moment from "moment"
 
 const Order = () => {
   const [orders, setOrders] = useState([])
+  const [statusValues, setStatusValues] = useState([])
 
   const { user, token } = isAuthenticated()
 
@@ -18,13 +19,22 @@ const Order = () => {
         console.log("errors", data.error)
       } else {
         setOrders(data)
-        console.log(orders)
+      }
+    })
+  }
+  const loadStatusValues = () => {
+    getStatusValues(user._id, token).then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        setStatusValues(data)
       }
     })
   }
 
   useEffect(() => {
     loadOrders()
+    loadStatusValues()
   }, [])
 
   const showOrdersLength = () => {
@@ -48,14 +58,37 @@ const Order = () => {
     </div>
   )
 
+  const handleStatusChange = (e, orderid) => {
+    console.log("update product status")
+  }
+
+  const showStatus = (o) => (
+    <div className="form-group">
+      <h3 className="mark mb-4">Status: {o.status}</h3>
+      <select
+        className="form-control"
+        onChange={(e) => handleStatusChange(e, o._id)}
+      >
+        <option>Update Status</option>
+        {statusValues.map((status, index) => (
+          <option key={index} value={status}>
+            {status}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+
   return (
     <Layout
       title="Orders"
-      description={`Hello ${user.name},you can manage all the orders here`}
+      description={`G'day ${user.name}, you can manage all the orders here`}
+      className="container-fluid"
     >
       <div className="row">
         <div className="col-md-8 offset-md-2">
           {showOrdersLength()}
+
           {orders.map((o, oIndex) => {
             return (
               <div
@@ -64,42 +97,46 @@ const Order = () => {
                 style={{ borderBottom: "5px solid indigo" }}
               >
                 <h2 className="mb-5">
-                  <span className="bg-primary">Order Id : {o._id}</span>
+                  <span className="bg-primary">Order ID: {o._id}</span>
                 </h2>
+
                 <ul className="list-group mb-2">
-                  <li className="list-group-item">{o.status}</li>
-                  <li className="list-group-item">{o.transaction_id}</li>
-                  <li className="list-group-item">$ {o.amount}</li>
+                  <li className="list-group-item">{showStatus(o)}</li>
                   <li className="list-group-item">
-                    Ordered By : {o.user.name}
+                    Transaction ID: {o.transaction_id}
+                  </li>
+                  <li className="list-group-item">Amount: ${o.amount}</li>
+                  <li className="list-group-item">Ordered by: {o.user.name}</li>
+                  <li className="list-group-item">
+                    Ordered on: {moment(o.createdAt).fromNow()}
                   </li>
                   <li className="list-group-item">
-                    Oredered on : {moment(o.createdAt).fromNow()}
-                  </li>
-                  <li className="list-group-item">
-                    Delivery address : {o.address}
+                    Delivery address: {o.address}
                   </li>
                 </ul>
 
                 <h3 className="mt-4 mb-4 font-italic">
-                  Total products in order :{o.products.length}
+                  Total products in the order: {o.products.length}
                 </h3>
+
                 {o.products.map((p, pIndex) => (
                   <div
-                    key={pIndex}
                     className="mb-4"
-                    style={{ padding: "20px", border: "1px solid Indigo" }}
+                    key={pIndex}
+                    style={{
+                      padding: "20px",
+                      border: "1px solid indigo",
+                    }}
                   >
                     {showInput("Product name", p.name)}
                     {showInput("Product price", p.price)}
-                    {showInput("Product count", p.count)}
+                    {showInput("Product total", p.count)}
                     {showInput("Product Id", p._id)}
                   </div>
                 ))}
               </div>
             )
           })}
-          {loadOrders()}
         </div>
       </div>
     </Layout>

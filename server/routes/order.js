a2f0642b897437fb6aed2c4ece63082d42c588ff
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-21 21:54:30 +0530

    created a function to list all the orders by a user

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const router = express.Router()
-const { requireSignin, isAuth } = require("../controllers/auth")
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth")
 const { userById, addOrderToUserhistory } = require("../controllers/user")
 const { decreaseQuantity } = require("../controllers/product")
-const { create } = require("../controllers/order")
+const { create, listOrders } = require("../controllers/order")
 
 router.post(
   "/order/create/:userId",
@@ -13,6 +13,8 @@ router.post(
   decreaseQuantity,
   create
 )
+
+router.get('/order/list/"userId', requireSignin, isAuth, isAdmin, listOrders)
 
 router.param("userId", userById)
 module.exports = router

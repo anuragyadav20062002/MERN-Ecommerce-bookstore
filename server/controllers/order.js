ba42c6ef2981dc317f9f9aaf07ed97b21c2efcd6
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-21 23:48:44 +0530

    Created a function to update order status by id

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -47,3 +47,19 @@ exports.orderById = (req, res, next, id) => {
       next()
     })
 }
+
+exports.updateOrderStatus = (req, res) => {
+  Order.update(
+    { _id: req.body.orderId },
+    { $set: { status: req.body.status } },
+    (err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        })
+      }
+
+      res.json(order)
+    }
+  )
+}

e9a4acbd04a5a4fa0a0b66553de15839ee13a3de
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-22 12:51:21 +0530

    added a controller to show user purchase history

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,12 @@ const express = require("express")
 const router = express.Router()
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth")
 
-const { userById, read, update } = require("../controllers/user")
+const {
+  userById,
+  read,
+  update,
+  purchaseHistory,
+} = require("../controllers/user")
 
 //Routes
 router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
@@ -13,6 +18,7 @@ router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
 
 router.get("/user/:userId", requireSignin, isAuth, read)
 router.put("/user/:userId", requireSignin, isAuth, update)
+router.get("/orders/by/:userId", requireSignin, isAuth, purchaseHistory)
 
 router.param("userId", userById)
 

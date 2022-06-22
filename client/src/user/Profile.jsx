fa9f4545fc2642ed80df522d8e04e7b711f76356
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-06-22 12:44:25 +0530

    successfully updating data from frontend

--- client/src/user/Profile.jsx
+++ client/src/user/Profile.jsx
@@ -34,15 +34,82 @@ const Profile = ({ match }) => {
     init(match.params.userId)
   }, [])
 
+  const handleChange = (name) => (e) => {
+    setValues({ ...values, error: false, [name]: e.target.value })
+  }
+
+  const clickSubmit = (e) => {
+    e.preventDefault()
+    update(match.params.userId, token, { name, email, password }).then(
+      (data) => {
+        if (data.error) {
+          // console.log(data.error);
+          alert(data.error)
+        } else {
+          updateuser(data, () => {
+            setValues({
+              ...values,
+              name: data.name,
+              email: data.email,
+              success: true,
+            })
+          })
+        }
+      }
+    )
+  }
+
+  const redirectUser = (success) => {
+    if (success) {
+      return <Redirect to="/cart" />
+    }
+  }
+
+  const profileUpdate = (name, email, password) => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          type="text"
+          onChange={handleChange("name")}
+          className="form-control"
+          value={name}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          type="email"
+          onChange={handleChange("email")}
+          className="form-control"
+          value={email}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          type="password"
+          onChange={handleChange("passwod")}
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  )
+
   return (
     <>
       <Layout
         title={`${name}'s Profile`}
         description={`Hey ${name} You can Update your profile here`}
-        className="container-fluid"
+        className="container"
       >
         <h2>Profile Update</h2>
-        {JSON.stringify(values)}
+        {profileUpdate(name, email, password)}
+        {redirectUser(success)}
       </Layout>
     </>
   )
